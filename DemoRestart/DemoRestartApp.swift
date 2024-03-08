e4a7cac1d455b9dfa2116232470ea6da0c270b4e
cesar <cursos.mapatupisaro@gamail.com>
2024-03-08 17:07:16 -0600

    Initial Commit

---
+++ DemoRestart/DemoRestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRestartApp.swift
+//  DemoRestart
+//
+//  Created by Aguid Ramirez Sanchez on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoRestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
