46ca34fc85eac0dce537c31c640e1673887e0776
cesar <cursos.mapatupisaro@gamail.com>
2024-03-09 23:23:15 -0600

    add animation to onboardingview

--- DemoRestart/Screens/OnboardingView.swift
+++ DemoRestart/Screens/OnboardingView.swift
@@ -13,6 +13,10 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+//    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+    @State private var imageoffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     //MARK: - BODY
     var body: some View {
@@ -24,10 +28,12 @@ struct OnboardingView: View {
                 //MARK: - HEADER
                 Spacer()
                 VStack(spacing:0){
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+//                        .id(textTitle)
                     Text("""
                             It's not how much we give but
                             how much love we put into giving.
@@ -37,6 +43,7 @@ struct OnboardingView: View {
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal,10)
+                
                     
                 }//: HEADER
                 .opacity(isAnimating ? 1 : 0)
@@ -44,15 +51,49 @@ struct OnboardingView: View {
                 .animation(.easeOut(duration: 1), value: isAnimating)
                 //MARK: - CENTER
                 ZStack{
-                    ZStack{
-                        CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
-                    }//: ZTACK
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageoffset.width * -1)
+                        .blur(radius: abs(imageoffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageoffset)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
-                }
+                        .offset(x: imageoffset.width * 1.2, y:0)
+                        .rotationEffect(.degrees(Double(imageoffset.width / 10)))
+                        .gesture(
+                        DragGesture()
+                            .onChanged({ gesture in
+                                if abs(imageoffset.width) <= 150{
+                                    imageoffset = gesture.translation
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 0
+                                        textTitle = "Give."
+                                    }
+                                }
+                            })
+                            .onEnded { _ in
+                                imageoffset = .zero
+                                withAnimation(.linear(duration: 0.25)){
+                                    indicatorOpacity = 1
+                                    textTitle = "Share."
+                                }
+                            }
+                        ) //: GESTURE
+                        .animation(.easeOut(duration: 1), value: imageoffset)
+                }//: CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    ,alignment: .bottom
+                )
+                
                 Spacer()
                 //MARK: - FOOTER
                 ZStack{
